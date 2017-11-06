fix bug in entropy calculation

entropy.py
@@ -16,7 +16,7 @@
 import numpy as np
 
 img_bins = np.arange(0, 256)
-hidden_bins = np.arange(0, 30)
+hidden_bins = np.arange(1, 31)
 digitize_bins = np.linspace(-1, 1, 30)
 
 
@@ -62,3 +62,5 @@ def _get_entropy(band, bins):
 
 def _entropy(probs):
     return -(np.log2(probs) * probs).sum()
+
+# im = imread('ex.jpg',as_grey=True)

plot.py
@@ -12,8 +12,11 @@
 @desc:
 '''
 
-import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('Agg')
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def create_color_bar(f, cmap, max_epochs, title='Epochs'):
@@ -51,7 +54,8 @@ def plot_info_plain(IXT, ITY):
     plt.xlabel("I(X;T)")
     plt.ylabel("I(T;Y)")
     create_color_bar(f, cmap, len(IXT))
-    plt.show()
+    # plt.show()
     plt.savefig('./fig.png')
+
 
 plot_info_plain(np.asarray([[1, 2, 4, 8, 16]]), np.asarray([[2, 3, 4, 5, 6]]))